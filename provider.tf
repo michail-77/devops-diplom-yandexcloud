dece85fef8fad6a869dfaeb0fc98c35df875408b
mikhail <michail.77@mail.ru>
2024-04-17 15:20:33 +0300

    1

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+# Provider
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
 provider "yandex" {
   # Укажите вашу авторизационную информацию
   token     = "${var.token}"
