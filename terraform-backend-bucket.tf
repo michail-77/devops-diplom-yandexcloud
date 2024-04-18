eaaa67e5dfbcdcd59f086a1ecf9b9fb83fa60437
Mikhail <michail.77@mail.ru>
2024-04-18 21:18:36 +0300

    1

--- terraform-backend-bucket.tf
+++ terraform-backend-bucket.tf
@@ -11,7 +11,7 @@ terraform {
     skip_region_validation = true
     skip_credentials_validation = true
 #    skip_metadata_api_check = true
-    access_key = "<идентификатор_ключа>"
-    secret_key = "<секретный_ключ>"
+    access_key = var.access_key
+    secret_key = var.secret_key
   }
 }
