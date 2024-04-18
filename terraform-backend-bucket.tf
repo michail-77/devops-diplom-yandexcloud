98cc73958788f5f81437e3a190a485e5ba380a4b
mikhail <michail.77@mail.ru>
2024-04-18 15:29:49 +0300

    1

--- terraform-backend-bucket.tf
+++ terraform-backend-bucket.tf
@@ -2,12 +2,14 @@
 
 terraform {
   backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
     bucket         = "terraform-backend-bucket"
     key            = "terraform.tfstate"
     region         = "ru-central1"
-    endpoint.s3       = "https://storage.yandexcloud.net/"
     skip_region_validation = true
     skip_credentials_validation = true
-    skip_metadata_api_check = true
+#    skip_metadata_api_check = true
   }
 }
