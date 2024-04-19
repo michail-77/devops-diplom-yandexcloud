41a84f172b43a1ad38b164c86621945265660d9b
mikhail <michail.77@mail.ru>
2024-04-19 16:58:43 +0300

    1

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-# Заменить на ID своего облака
-# https://console.cloud.yandex.ru/cloud?section=overview
-variable "yandex_cloud_id" {
-  default = var.cloud_id
-}
-
-# Заменить на Folder своего облака
-# https://console.cloud.yandex.ru/cloud?section=overview
-variable "yandex_folder_id" {
-  default = var.folder.id
-}
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
