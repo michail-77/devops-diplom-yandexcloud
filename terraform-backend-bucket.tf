21c23fe273baf05fff5c3486121846bb8357d420
mikhail <michail.77@mail.ru>
2024-04-19 14:33:24 +0300

    1

--- terraform-backend-bucket.tf
+++ terraform-backend-bucket.tf
@@ -9,7 +9,7 @@ terraform {
     skip_region_validation = true
     skip_credentials_validation = true
 #    skip_metadata_api_check = true
-    access_key = yandex_iam_service_account_static_access_key.bucket-static_access_key.access_key
-    secret_key = yandex_iam_service_account_static_access_key.bucket-static_access_key.secret_key
+    access_key = "YCAJEeezftGa7PZZHVXXuhN-l"  #yandex_iam_service_account_static_access_key.bucket-static_access_key.access_key""
+    secret_key = "YCPJ9i8UuiA2gePRot1lG8srwWcRrtnLeVGte_hE" #yandex_iam_service_account_static_access_key.bucket-static_access_key.secret_key
   }
 }
