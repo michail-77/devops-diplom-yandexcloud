2e76b883ae0e4537bb8301b9e9e6f37e5aa4b3db
mikhail <michail.77@mail.ru>
2024-04-23 15:31:07 +0300

    1

--- variables.tf
+++ variables.tf
@@ -11,6 +11,10 @@ variable "folder_id" {
   type = string
 }
 
+variable "service_account_id" {
+  type = string
+}
+
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
