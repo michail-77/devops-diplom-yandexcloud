44e9f19f044b0b80c60c83312814ca6ac8ce868e
mikhail <michail.77@mail.ru>
2024-04-23 17:56:29 +0300

    1

--- provider.tf
+++ provider.tf
@@ -12,8 +12,8 @@ provider "yandex" {
   # Укажите вашу авторизационную информацию
   service_account_key_file = "authorized_key.json"
   token     = var.token
-  cloud_id  = "${var.cloud_id}"
-  folder_id = "${var.folder_id}"
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
   zone      = "ru-central1-a"       # Или другая зона по вашему выбору
 }
 
