dc64ed1634f35f65e61d4ef23de8dab014202ec7
mikhail <michail.77@mail.ru>
2024-04-23 17:51:16 +0300

    1

--- provider.tf
+++ provider.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "yandex" {
   # Укажите вашу авторизационную информацию
-#  service_account_key_file = "key.json"
+  service_account_key_file = "key.json"
   token     = var.token
   cloud_id  = "${var.cloud_id}"
   folder_id = "${var.folder_id}"
-  zone      = "ru-central1-a" # Или другая зона по вашему выбору
+  zone      = "ru-central1-a"       # Или другая зона по вашему выбору
 }
 
