ce892771cf8250c14a1d7e23d17ee774639c0a6e
Mikhail <michail.77@mail.ru>
2024-04-25 21:50:29 +0300

    1

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
-resource "yandex_vpc_network" "yc-network" {
-  name        = "yc-network"
-  description = "My first network"
-}
+# resource "yandex_vpc_network" "yc-network" {
+#   name        = "yc-network"
+#   description = "My first network"
+# }
 
 
 # resource "yandex_vpc_network" "network" {
@@ -19,33 +19,34 @@ resource "yandex_vpc_network" "yc-network" {
 # }
 
 
-# # Создание виртуальной частной сети (VPC)
-# resource "yandex_vpc_network" "my_vpc_network" {
-#   name          = "my-vpc-network"
-#   description   = "VPC network for my infrastructure"
-# }
+# Создание виртуальной частной сети (VPC)
+resource "yandex_vpc_network" "my_vpc_network" {
+  name          = "my-vpc-network"
+  description   = "VPC network for my infrastructure"
+  folder_id     = var.folder_id
+}
 
-# # Создание подсетей в разных зонах доступности
-# # resource "yandex_vpc_subnet" "subnet_zone_a" {
-# #   name           = "subnet-zone-a"
-# #   network_id     = yandex_vpc_network.my_vpc_network.id
-# #   zone           = "ru-central1-a"
-# #   v4_cidr_blocks = ["10.10.1.0/24"]
-# # }
+# Создание подсетей в разных зонах доступности
+resource "yandex_vpc_subnet" "subnet_zone_a" {
+  name           = "subnet-zone-a"
+  network_id     = yandex_vpc_network.my_vpc_network.id
+  zone           = "ru-central1-a"
+  v4_cidr_blocks = ["10.10.1.0/24"]
+}
 
-# # resource "yandex_vpc_subnet" "subnet_zone_b" {
-# #   name           = "subnet-zone-b"
-# #   network_id     = yandex_vpc_network.my_vpc_network.id
-# #   zone           = "ru-central1-b"
-# #   v4_cidr_blocks = ["10.10.2.0/24"]
-# # }
+resource "yandex_vpc_subnet" "subnet_zone_b" {
+  name           = "subnet-zone-b"
+  network_id     = yandex_vpc_network.my_vpc_network.id
+  zone           = "ru-central1-b"
+  v4_cidr_blocks = ["10.10.2.0/24"]
+}
 
-# # resource "yandex_vpc_subnet" "subnet_zone_c" {
-# #   name           = "subnet-zone-d"
-# #   network_id     = yandex_vpc_network.my_vpc_network.id
-# #   zone           = "ru-central1-d"
-# #   v4_cidr_blocks = ["10.10.3.0/24"]
-# # }
+resource "yandex_vpc_subnet" "subnet_zone_c" {
+  name           = "subnet-zone-d"
+  network_id     = yandex_vpc_network.my_vpc_network.id
+  zone           = "ru-central1-d"
+  v4_cidr_blocks = ["10.10.3.0/24"]
+}
 
 # variable "subnet-zones" {
 #  type    = list(string)
