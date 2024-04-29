ab2136c61b871f7d4c37b66d256da95406c57ca2
Mikhail <michail.77@mail.ru>
2024-04-29 21:47:44 +0300

    1

--- terraform/my-cluster.tf
+++ terraform/my-cluster.tf
@@ -30,42 +30,39 @@ resource "yandex_kubernetes_cluster" "k8s_cluster" {
  network_id = yandex_vpc_network.my_vpc_network.id
  master {
    master_location {
-     zone      = yandex_vpc_subnet.<имя_подсети>.zone
-     subnet_id = yandex_vpc_subnet.<имя_подсети>.id
+     zone      = yandex_vpc_subnet.subnet_zone_a.zone
+     subnet_id = yandex_vpc_subnet.subnet_zone_a.id
    }
  }
- service_account_id      = yandex_iam_service_account.<имя_сервисного_аккаунта>.id
- node_service_account_id = yandex_iam_service_account.<имя_сервисного_аккаунта>.id
+ service_account_id      = yandex_iam_service_account.sa.id
+ node_service_account_id = yandex_iam_service_account.sa.id
    depends_on = [
-     yandex_resourcemanager_folder_iam_member.editor,
+     yandex_resourcemanager_folder_iam_member.sa-editor,
      yandex_resourcemanager_folder_iam_member.images-puller
    ]
 }
 
-resource "yandex_vpc_network" "<имя_сети>" { name = "<имя_сети>" }
-
-resource "yandex_vpc_subnet" "<имя_подсети>" {
- v4_cidr_blocks = ["<диапазон_адресов_подсети>"]
- zone           = "<зона_доступности>"
- network_id     = yandex_vpc_network.<имя_сети>.id
-}
-
-resource "yandex_iam_service_account" "<имя_сервисного_аккаунта>" {
- name        = "<имя_сервисного_аккаунта>"
+resource "yandex_iam_service_account" "sa" {
+ name        = "service-account"
  description = "<описание_сервисного_аккаунта>"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "editor" {
+data "yandex_resourcemanager_folder" "department1" {
+  folder_id = "some_folder_id"
+} 
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
  # Сервисному аккаунту назначается роль "editor".
- folder_id = "<идентификатор_каталога>"
+ folder_id = var.folder_id
  role      = "editor"
- member    = "serviceAccount:${yandex_iam_service_account.<имя_сервисного_аккаунта>.id}"
+ member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
  # Сервисному аккаунту назначается роль "container-registry.images.puller".
- folder_id = "<идентификатор_каталога>"
+ folder_id = var.folder_id
  role      = "container-registry.images.puller"
- member    = "serviceAccount:${yandex_iam_service_account.<имя_сервисного_аккаунта>.id}"
+ member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
 }
+
 
